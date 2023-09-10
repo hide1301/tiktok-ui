be3511be28ba2337dd0d8b035f9d61f054844d6b
Thanh Nghi <thanhnghi1301@gmail.com>
2023-09-10 11:48:17 +0700

    Xây Dựng UI Phần Header Dự Án Tiktok #5

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -1,14 +1,40 @@
+import { useEffect, useState } from 'react'
 import className from 'classnames/bind'
 import Tippy from '@tippyjs/react/headless'
+
 import { Wrapper as PopperWrapper } from '~/components/Popper'
-import styles from './Menu.module.scss'
 import MenuItem from './MenuItem'
+import Header from './Header'
+import styles from './Menu.module.scss'
 
 const cx = className.bind(styles)
 
-function Menu({ children, items = [] }) {
+const defaultFn = () => {}
+
+function Menu({ children, items = [], onChange = defaultFn }) {
+    const [history, setHistory] = useState([{ data: items }])
+    const current = history[history.length - 1] // Lấy phần tử cuối cùng của mảng
+
     const renderItems = () => {
-        return items.map((item, index) => <MenuItem key={index} data={item} />)
+        return current.data.map((item, index) => {
+            // Kiểm tra xem phần tử có children ko
+            const isParent = !!item.children
+
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory((prev) => [...prev, item.children]) // Push thêm một phần tử (children) vào cuối mảng
+                        } else {
+                            // Nếu không có children thì khi click vào sẽ xữ lý logic
+                            onChange(item)
+                        }
+                    }}
+                />
+            )
+        })
     }
     return (
         <Tippy
@@ -17,7 +43,17 @@ function Menu({ children, items = [] }) {
             placement="bottom-end"
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper className={cx('menu-popper')}>{renderItems()}</PopperWrapper>
+                    <PopperWrapper className={cx('menu-popper')}>
+                        {history.length > 1 && (
+                            <Header
+                                title="Language"
+                                onBack={() => {
+                                    setHistory((prev) => prev.slice(0, prev.length - 1))
+                                }}
+                            />
+                        )}
+                        {renderItems()}
+                    </PopperWrapper>
                 </div>
             )}
         >
